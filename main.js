64f4b1d07bca27e60ce2c934c373d1b1e3a04ec7
Anju0307 <jropero111@gmail.com>
2022-10-24 20:13:33 -0500

    punto 2

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 addEventListener("DOMContentLoaded", (e) => {
-    let form = document.querySelector('#form');
-    let suma = 0;
+    let suma = 0,des;
+    let form = document.querySelector("#form");
+    document.querySelector("#numero1").readOnly = false;
+    document.querySelector("#boton").disabled = false;
+    let resultado = document.querySelector("#resultado");
+    let deshabilitar = () => {
+        document.querySelector("#numero1").readOnly = true;
+        document.querySelector("#boton").disabled = true;
+    }
+    let descuento = () => {
+        (suma>10000)
+            ? (des =suma-(suma*0.10))
+            (resultado.insertAdjacentHTML("beforebegin", `Su descuentoes del 10% y su total a pagar fue: ${des}`))
+            : (resultado.insertAdjacentHTML("beforebegin", `${suma}`));
+
+    }
     form.addEventListener("submit", (e) => {
         e.preventDefault();
-        let dato =parseInt(document.querySelector('#dato').value);
-        if(dato > 0){
-            suma += dato;
-        }
-        else{
-            alert(`porfavor solo ingresar números positivos`);
-            document.querySelector('#res').insertAdjacentHTML("beforeend",suma);
-        }
+        let num1 = parseInt(document.querySelector("#numero1").value);
         form.reset();
+        (num1 > 0)
+            ? (suma += num1)
+            : (num1 < 0) ?
+                (alert("No puedes ingresar un numero menor"))
+                : (deshabilitar())
+                    (descuento());
     })
-
 })
